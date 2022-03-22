15e55b7d2e88bb05dcd935f3f3f8c4c51b85c52f
Alejo Aragon <alejo.aragon.05@gmail.com>
2022-03-21 22:38:38 -0500

    complex

---
+++ 0x00-math_complex/8-complex.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <math.h>
+/**
+ * complex_from_mod_arg - function that update the real and the
+ * imaginary parts of
+ * a complex number given its modulus and arguments.
+ * @m: Parameter
+ * @arg: Parameter
+ * @c3: Parameter pointer to structure
+ * Return: return the multiplication
+ */
+void complex_from_mod_arg(double m, double arg, complex *c3)
+{
+	c3->re = m * cos(arg);
+	c3->im = m * sin(arg);
+}
